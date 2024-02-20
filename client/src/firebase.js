35457602205a28353f70a9df5c77511fd1b7eee4
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-02-20 21:38:17 +0500

    not created the google authentication

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:"AIzaSyA6OM8VHwPetVYpGeVL9GUR5ABHyh4kQJU" ,
+//   authDomain: "mern-estate-8adb0.firebaseapp.com",
+//   projectId: "mern-estate-8adb0",
+//   storageBucket: "mern-estate-8adb0.appspot.com",
+//   messagingSenderId: "471081706322",
+//   appId: "1:471081706322:web:3efbe0ae94d97c2c28508b"
+// };
+
+// // Initialize Firebase
+//  export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { Link , useNavigate } from "react-router-dom";
 import { useState } from "react";
+// import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [formData , setFormData] = useState({});
@@ -66,6 +67,7 @@ export default function SignUp() {
           {" "}
           {loding ? 'loding...' : 'Sing Up'}
         </button>
+        {/* <OAuth/> */}
       </form>
       <div className=" flex gap-2 mt-5">
         {" "}
